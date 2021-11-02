60b9d20500a56329b839283f827d9cc059919fa1
Tony Collins <tjc@eltham-college.org.uk>
2021-11-02 22:03:52 +0000

    ....

--- Sevens/App.xaml.cs
+++ Sevens/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Sevens
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class aApp : Application
     {
     }
 }
