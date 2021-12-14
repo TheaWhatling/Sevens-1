09d3aff486ad9b661f1851a3d1a523f7f060639e
Thea Whatling <K038218@eltham-college.org.uk>
2021-12-14 12:20:25 +0000

    deck

---
+++ Sevens/Deck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sevens
+{
+    class Deck
+    {
+        private Card[] cards;
+        public int FrontPointer;
+        public const int decksize = 52;
+
+        public Deck() 
+        {
+            cards = new Card[decksize];
+            FrontPointer = 0;
+
+            int counter = 0;
+
+            for (int CounterSuit = 0; CounterSuit < 4; CounterSuit++)
+            {
+                for (int valueCounter = 2; valueCounter < 15; valueCounter++)
+                {
+                    cards[counter] = new Card(valueCounter, CounterSuit);
+                    counter++;
+                }
+            }
+        }
+
+        //shuffle
+    }
+}
--- Sevens/Game.cs
+++ Sevens/Game.cs
@@ -6,6 +6,7 @@ namespace Sevens
 	{
 		public Game()
 		{
+
 		}
 	}
 }
